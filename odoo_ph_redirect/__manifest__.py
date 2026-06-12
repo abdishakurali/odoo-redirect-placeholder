ea4e15afea9c18dd0d45403adaee6924ec58b0cf
abdishakuuralimohamed <abdishakuuralimohamed@gmail.com>
2026-06-12 23:12:33 +0100

    Add placeholder module to release fiscal_cash_register name

---
+++ odoo_ph_redirect/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': 'Redirect Placeholder',
+    'version': '17.0.1.0.0',
+    'category': 'Technical',
+    'summary': 'Placeholder module',
+    'installable': False,
+    'application': False,
+}
